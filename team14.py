e842ea458d9d478091a96a4e58fef853536fce5a
Amy Dreher <aemberson44@gmail.com>
2019-02-19 20:14:45 +0000

    Strategy

--- team14.py
+++ team14.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Dreher' # Only 10 chars displayed.
+team_name = 'Testing Team' # Only 10 chars displayed.
 strategy_name = 'Always Betray'
 strategy_description = 'How does this strategy decide?'
     
